183cf2b6e43301b06199129f19d86e7ce276e65a
Francis Jesmar Montalbo <65653238+francismontalbo@users.noreply.github.com>
2024-05-06 20:41:33 +0800

    main for the final exam sample

---
+++ sample_structure/main.py
@@ -0,0 +1,10 @@
+from src.number_guesser import NumberGuesser
+
+
+def main():
+    print("Welcome to Number Guesser!")
+    game = NumberGuesser()
+    game.start()
+
+if __name__ == "__main__":
+    main()
